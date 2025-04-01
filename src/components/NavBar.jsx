364f88e4e0b82a6180244d2aa4393bd00dc6a810
Gantedavamsikrishna <vamsiganteda@gmail.com>
2025-04-01 22:46:13 +0530

    Update Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
+import "./NavBar.css";
 function NavBar() {
   return (
-    <nav>
-      <h1>Navigation Bar</h1>
+    <nav className="style">
+      <div className="logo">
+        <h1>
+          <strong>Logo</strong>
+        </h1>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Blog</li>
+        <li>conatct</li>
+        <li>login</li>
+      </ul>
     </nav>
   );
 }
